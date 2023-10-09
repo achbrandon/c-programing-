4fc653c825655b6b8aebf5719787a2206217341f
Ultimate <ultimate@Ultimates-MacBook-Pro.local>
2023-10-09 12:23:24 +0300

    introduction to c programing

---
+++ tuturials/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+ 
+int main ()
+{
+   char characterName[] = "angela";
+
+    int Age =50;
+    printf("i love %s and she is %d years old\n", characterName, Age);
+    printf("i love mr %s\n" , characterName );
+    printf("she is %s , and  she is %d\n", characterName, Age);
+    printf("%s is so beautiful but she is a young girl of age %d\n", characterName, Age);
+    printf("this programing language is so interetsing my name is %s\n", characterName);
+    
+ return 0; 
+  
+}
